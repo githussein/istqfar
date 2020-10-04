a89e22fe30a846455746b1325a9428c7ede330a9
githussein <mo.samy.hussein@gmail.com>
2020-10-04 18:14:22 +0200

    New home page

--- lib/counter.dart
+++ lib/counter.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Counter extends StatefulWidget {
   final String zekrText;
@@ -12,6 +13,7 @@ class _CounterState extends State<Counter> {
   int _counter = 0;
   int _totalCounter = 0;
   int _round = 1;
+  int storedValue = 0;
 
   //Method to increment Zekr counter
   void _incrementCounter() {
@@ -28,114 +30,147 @@ class _CounterState extends State<Counter> {
   }
 
   @override
+  void initState() {
+    //Read app settings and azkar counts from SharedPreferences
+    _read();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[200],
-      appBar: AppBar(
-        title: Text("المسبحة"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.all(10.0),
-              padding: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 10.0),
-              decoration: BoxDecoration(
-                  color: Colors.brown[400],
-                  border: Border.all(
-                    color: Colors.brown[900],
-                  ),
-                  borderRadius: BorderRadius.all(Radius.circular(45))),
-              child: Text(
-                widget.zekrText,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 22.0,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-            Column(
-              children: <Widget>[
-                SizedBox(
-                  height: 5.0,
-                  child: Divider(
-                    color: Colors.brown[900],
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      '$_totalCounter ',
-                      style: Theme.of(context).textTheme.headline6,
+    //Override the back button to save numbers of azkar
+    return WillPopScope(
+      onWillPop: () {
+        _save();
+
+        //trigger leaving and use own data
+        Navigator.pop(context, false);
+
+        //we need to return a future
+        return Future.value(false);
+      },
+      child: Scaffold(
+        backgroundColor: Colors.brown[200],
+        appBar: AppBar(
+          title: Text("المسبحة"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 10.0),
+                decoration: BoxDecoration(
+                    color: Colors.brown[400],
+                    border: Border.all(
+                      color: Colors.brown[900],
                     ),
-                    Text(
-                      'العدد الكلي ',
-                      style: TextStyle(
-                          fontSize: 18.0, fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 5.0,
-                  child: Divider(
-                    color: Colors.brown[900],
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      '$_round',
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                    Text(
-                      'الدورة ',
-                      style: TextStyle(
-                          fontSize: 18.0, fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 5.0,
-                  child: Divider(
-                    color: Colors.brown[900],
-                  ),
-                ),
-              ],
-            ),
-            Column(
-              children: <Widget>[
-                Text(
-                  'العدد الحالي',
-                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  '$_counter',
+                    borderRadius: BorderRadius.all(Radius.circular(45))),
+                child: Text(
+                  widget.zekrText,
+                  textAlign: TextAlign.center,
                   style: TextStyle(
-                      fontSize: 48,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.brown),
-                ),
-              ],
-            ),
-            FlatButton(
-              onPressed: _incrementCounter,
-              child: CircleAvatar(
-                radius: 100.0,
-                backgroundColor: Colors.brown,
-                child: CircleAvatar(
-                  radius: 95.0,
-                  backgroundImage: AssetImage('images/seb7ah.jpg'),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 22.0,
+                    color: Colors.white,
+                  ),
                 ),
               ),
-            ),
-          ],
+              Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 5.0,
+                    child: Divider(
+                      color: Colors.brown[900],
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        '$_totalCounter ',
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                      Text(
+                        'العدد الكلي ',
+                        style: TextStyle(
+                            fontSize: 18.0, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 5.0,
+                    child: Divider(
+                      color: Colors.brown[900],
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        '$_round',
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                      Text(
+                        'الدورة ',
+                        style: TextStyle(
+                            fontSize: 18.0, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 5.0,
+                    child: Divider(
+                      color: Colors.brown[900],
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Text(
+                    'العدد الحالي',
+                    style:
+                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    '$_totalCounter',
+                    style: TextStyle(
+                        fontSize: 48,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.brown),
+                  ),
+                ],
+              ),
+              FlatButton(
+                onPressed: _incrementCounter,
+                child: CircleAvatar(
+                  radius: 100.0,
+                  backgroundColor: Colors.brown,
+                  child: CircleAvatar(
+                    radius: 95.0,
+                    backgroundImage: AssetImage('images/seb7ah.jpg'),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  _read() async {
+    final prefs = await SharedPreferences.getInstance();
+    final key = 'my_int_key';
+    final value = prefs.getInt(key) ?? 0;
+    _totalCounter = value;
+    setState(() {});
+  }
+
+  _save() async {
+    final prefs = await SharedPreferences.getInstance();
+    final key = 'my_int_key';
+    prefs.setInt(key, _totalCounter);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:istqfar/counter.dart';
 import 'package:istqfar/choose_azkar.dart';
 import 'package:istqfar/misbaha.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
@@ -33,19 +32,19 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 0;
-  final List<Widget> _widgetOptions = [];
+  // final List<Widget> _widgetOptions = [];
 
-  FlutterLocalNotificationsPlugin fltrNotification;
+  FlutterLocalNotificationsPlugin flutterNotification;
 
   @override
   void initState() {
     super.initState();
-    var androidInitilize = new AndroidInitializationSettings('app_icon');
-    var iOSinitilize = new IOSInitializationSettings();
-    var initilizationsSettings =
-        new InitializationSettings(androidInitilize, iOSinitilize);
-    fltrNotification = new FlutterLocalNotificationsPlugin();
-    fltrNotification.initialize(initilizationsSettings,
+    var androidInitialize = new AndroidInitializationSettings('app_icon');
+    var iOSInitialize = new IOSInitializationSettings();
+    var initializationSettings =
+        new InitializationSettings(androidInitialize, iOSInitialize);
+    flutterNotification = new FlutterLocalNotificationsPlugin();
+    flutterNotification.initialize(initializationSettings,
         onSelectNotification: notificationSelected);
   }
 
@@ -67,11 +66,11 @@ class _MyHomePageState extends State<MyHomePage> {
     var generalNotificationDetails =
         new NotificationDetails(androidDetails, iSODetails);
 
-    // await fltrNotification.show(
+    // await flutterNotification.show(
     //     0, "Task", "You created a Task", generalNotificationDetails,
     //     payload: "Task");
     var scheduledTime = DateTime.now().add(Duration(hours: 1));
-    fltrNotification.schedule(1, "تنبيه بالذكر", "سبحان الله وبحمده",
+    flutterNotification.schedule(1, "تنبيه بالذكر", "سبحان الله وبحمده",
         scheduledTime, generalNotificationDetails);
   }
 
@@ -81,243 +80,123 @@ class _MyHomePageState extends State<MyHomePage> {
     _showNotification();
 
     return Scaffold(
-      backgroundColor: Colors.brown[200],
       appBar: AppBar(
         title: Text(widget.title),
       ),
       body: Container(
-        child: ListView(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/background.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Container(
-              margin: EdgeInsets.all(10.0),
-              padding: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 10.0),
-              decoration: BoxDecoration(
-                  color: Colors.brown[400],
-                  border: Border.all(
-                    color: Colors.brown[900],
-                  ),
-                  borderRadius: BorderRadius.all(Radius.circular(45))),
-              child: Column(
-                children: <Widget>[
-                  Text(
-                    'آيـة اليـوم',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 22.0,
-                      color: Colors.brown[900],
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    height: 150.0,
+                    margin: EdgeInsets.all(10.0),
+                    padding: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 10.0),
+                    decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.55),
+                        borderRadius: BorderRadius.all(Radius.circular(15))),
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => Misbaha()),
+                        );
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          ImageIcon(
+                            AssetImage('images/icon-tasbih.png'),
+                            size: 100.0,
+                          ),
+                          Text('سبحة')
+                        ],
+                      ),
                     ),
                   ),
-                  SizedBox(
-                    height: 10.0,
-                    child: Divider(
-                      color: Colors.brown[900],
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(10.0),
+                    padding: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 10.0),
+                    decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.55),
+                        borderRadius: BorderRadius.all(Radius.circular(15))),
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ChooseAzkar()),
+                        );
+                      },
+                      child: Column(
+                        children: <Widget>[
+                          ImageIcon(
+                            AssetImage('images/icon-dua.png'),
+                            size: 100.0,
+                          ),
+                          Text('أذكار')
+                        ],
+                      ),
                     ),
                   ),
-                  Text(
-                    'فَسَتَذْكُرُونَ مَا أَقُولُ لَكُمْ ۚ وَأُفَوِّضُ أَمْرِي إِلَى اللَّهِ ۚ إِنَّ اللَّهَ بَصِيرٌ بِالْعِبَادِ (44)',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18.0,
-                      color: Colors.white,
-                    ),
-                  ),
-                  Text(
-                    'غـافـر',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16.0,
-                      color: Colors.brown[900],
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
-            Container(
-              margin: EdgeInsets.all(10.0),
-              padding: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 10.0),
-              decoration: BoxDecoration(
-                  color: Colors.brown[400],
-                  border: Border.all(
-                    color: Colors.brown[900],
-                  ),
-                  borderRadius: BorderRadius.all(Radius.circular(45))),
-              child: Column(
-                children: <Widget>[
-                  Text(
-                    'حـديـث اليـوم',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 22.0,
-                      color: Colors.brown[900],
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(10.0),
+                    padding: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 10.0),
+                    decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.55),
+                        borderRadius: BorderRadius.all(Radius.circular(15))),
+                    child: Column(
+                      children: <Widget>[
+                        ImageIcon(
+                          AssetImage('images/icon-reading.png'),
+                          size: 100.0,
+                        ),
+                        Text('قصص')
+                      ],
                     ),
                   ),
-                  SizedBox(
-                    height: 10.0,
-                    child: Divider(
-                      color: Colors.brown[900],
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(10.0),
+                    padding: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 10.0),
+                    decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.55),
+                        borderRadius: BorderRadius.all(Radius.circular(15))),
+                    child: Column(
+                      children: <Widget>[
+                        ImageIcon(
+                          AssetImage('images/icon-mhmd.png'),
+                          size: 100.0,
+                        ),
+                        Text('أحاديث')
+                      ],
                     ),
                   ),
-                  Text(
-                    'أبشروا وأمّلوا ما يسركم، فوالله ما الفقر أخشى عليكم، ولكني أخشى أن تبسط الدنيا عليكم كما بسطت على من كان قبلكم، فتنافسوها كما تنافسوها، فتهلككم كما أهلكتهم',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18.0,
-                      color: Colors.white,
-                    ),
-                  ),
-                  Text(
-                    'مـتـفـق عـلـيـه',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16.0,
-                      color: Colors.brown[900],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.all(10.0),
-              padding: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 10.0),
-              decoration: BoxDecoration(
-                  color: Colors.brown[400],
-                  border: Border.all(
-                    color: Colors.brown[900],
-                  ),
-                  borderRadius: BorderRadius.all(Radius.circular(45))),
-              child: Column(
-                children: <Widget>[
-                  Text(
-                    'قـصـة الأسـبوع',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 22.0,
-                      color: Colors.brown[900],
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10.0,
-                    child: Divider(
-                      color: Colors.brown[900],
-                    ),
-                  ),
-                  Text(
-                    'سعد بن أبي وقاص، خال رسول الله صلى الله عليه وسلم',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18.0,
-                      color: Colors.white,
-                    ),
-                  ),
-                  Text(
-                    'نشأ سعد في قريش، واشتغل في بري السهام وصناعة القِسِيّ، وهذا عمل يؤهِّل صاحبه للائتلاف مع الرمي، وحياة الصيد والغزو، وكان يمضي وقته وهو يخالط شباب قريش وساداتهم، ويتعرف على الدنيا عن طريق الحجيج الوافد إلى مكة المكرمة في أيام الحج ومواسمها',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18.0,
-                      color: Colors.white,
-                    ),
-                  ),
-                  Text(
-                    'كان ممن دعاهم أبو بكر للإسلام، فأسلم t مبكرًا، وهو ابن سبع عشرة سنة. وبعد إسلامه تركت أمه الطعام ليعود إلى الكفر، فقال لها: تعلمين والله يا أماه، لو كانت لك مائة نفس فخرجت نفسًا نفسًا، ما تركت ديني هذا لشيء؛ فإن شئت فكلي، وإن شئت لا تأكلي. فحلفتْ ألا تكلمه أبدًا حتى يكفر بدينه، ولا تأكل ولا تشرب؛ فأنزل الله : {وَوَصَّيْنَا الإِنْسَانَ بِوَالِدَيْهِ حُسْنًا وَإِنْ جَاهَدَاكَ لِتُشْرِكَ بِي مَا لَيْسَ لَكَ بِهِ عِلْمٌ فَلاَ تُطِعْهُمَا إِلَيَّ مَرْجِعُكُمْ فَأُنَبِّئُكُمْ بِمَا كُنْتُمْ تَعْمَلُونَ} [العنكبوت: 8]',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18.0,
-                      color: Colors.white,
-                    ),
-                  ),
-                  Text(
-                    'وكان سعد بن أبي وقاص أحد الفرسان، وهو أول من رمى بسهمٍ في سبيل الله، وهو أحد الستة أصحاب الشورى',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18.0,
-                      color: Colors.white,
-                    ),
-                  ),
-                  Text(
-                    'لم تعد حياة سعد ضياعًا في أحاديث التجارة أو اللغو، ولم يعد عمله في صنع السهام والقِسِيّ من أجل الربح، بل أصبح عمله جميعه موجَّهًا لهدف واحد، هو نصرة الدين، وإعلاء كلمة الله، والجهاد في سبيله بالمال والنفس والأهل والعشيرة؛ فقد شهد مع النبي غزوة بُواط وكان يحمل لواءها، وغزوة أُحد، وكان الرسول يعتمده في بعض الأعمال الخاصة، مثل إرساله مع علي بن أبي طالب والزبير بن العوام رضي الله عنهما بمهمة استطلاعية عند ماء بدر، وعندما عقد صلح الحديبية كان سعد بن أبي وقاص أحد شهود الصلح',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18.0,
-                      color: Colors.white,
-                    ),
-                  ),
-                  Text(
-                    'تولى سعد بن أبي وقاص مهمة قيادة جيش المسلمين في أصعب مرحلة من مراحل الحرب في بلاد فارس والعراق، فاستطاع بفضل الله أولاً، ثم بكفاءته وقدرته القيادية وتوجيهات أمير المؤمنين وجيشٍ يملؤه الإيمان أن يهزم الفرس هزيمة ساحقة في القادسية',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18.0,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ],
         ),
       ),
-      endDrawer: Drawer(
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage("images/seb7ah.jpg"),
-                      fit: BoxFit.cover)),
-              child: Text(
-                'استغفار - Istqfar',
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-            ListTile(
-              title: Text('التنبيهات'),
-              leading: Icon(Icons.alarm_on),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: Text('حول التطبيق'),
-              leading: Icon(Icons.mobile_screen_share),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: Text('الإعدادات'),
-              leading: Icon(Icons.settings),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        ),
-      ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: _incrementCounter,
-      //   tooltip: 'Increment',
-      //   child: Icon(Icons.add),
-      // ),
       bottomNavigationBar: BottomNavigationBar(
         items: [
           BottomNavigationBarItem(
